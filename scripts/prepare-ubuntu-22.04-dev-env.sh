481b6c47a76d49c69afbf4ef1844171d6512b160
EOSIO-Taurus <eosio-taurus@bullish.com>
2023-06-15 12:42:34 +0800

    Improve scripts for preparing building environment (#11)

--- scripts/prepare-ubuntu-22.04-dev-env.sh
+++ scripts/prepare-ubuntu-22.04-dev-env.sh
@@ -9,7 +9,7 @@ if [[ "$(id -u)" != "0" ]]; then
 fi
 
 apt update -y
-apt install -y curl gnupg bzip2 python3-pip cmake libgmp-dev pkg-config colordiff libusb-1.0-0-dev libcurl4-openssl-dev tpm2-openssl libtpms-dev autoconf libtool-bin
+apt install -y curl gnupg bzip2 python3-pip cmake libgmp-dev pkg-config colordiff libusb-1.0-0-dev libcurl4-openssl-dev autoconf libtool-bin
 
 apt remove -y gcc g++
 apt autoremove -y
@@ -20,16 +20,32 @@ echo "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-13 main" > /etc/apt/so
 apt update -y
 apt install -y clang-13 libclang-13-dev lld-13 libc++-13-dev libc++abi-13-dev
 
-export PATH=/usr/lib/llvm-13/bin:$PATH
+apt install -y llvm-11
+
+export PATH=/usr/lib/llvm-13/bin/:/usr/lib/llvm-11/bin:$PATH
 export CC=/usr/lib/llvm-13/bin/clang
 
-apt install -y llvm-11
+rm /usr/local/include/boost/ -rf
+rm /usr/local/lib/libboost_* -f
+
+export BOOST_VERSION=1_78_0
+export BOOST_VERSION_DOT=1.78.0
+export BOOST_CHECKSUM=8681f175d4bdb26c52222665793eef08490d7758529330f98d3b29dd0735bccc
+rm -rf "boost_${BOOST_VERSION}"
+curl -fsSLOk "https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION_DOT}/source/boost_${BOOST_VERSION}.tar.bz2" && \
+    echo "${BOOST_CHECKSUM} boost_${BOOST_VERSION}.tar.bz2" | sha256sum -c - && \
+    tar -xjf "boost_${BOOST_VERSION}.tar.bz2" && \
+    cd "boost_${BOOST_VERSION}" && \
+    ./bootstrap.sh --prefix=/usr/local && \
+    ./b2 --with-iostreams --with-date_time --with-filesystem --with-system --with-program_options --with-chrono --with-test -j$(nproc) install && \
+    cd .. && \
+    rm -rf "boost_${BOOST_VERSION}.tar.bz2" "boost_${BOOST_VERSION}"
 
 export PROTOBUF_VERSION_SHORT=21.5
 export PROTOBUF_VERSION=3.21.5
 export PROTOBUF_CHECKSUM=58c8a18b4ec22655535c493155c5465a8903e8249094ceead87e00763bdbc44f
 rm -rf "protobuf-${PROTOBUF_VERSION}"
-curl -fsSLO https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION_SHORT}/protobuf-cpp-${PROTOBUF_VERSION}.tar.gz && \
+curl -fsSLOk https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION_SHORT}/protobuf-cpp-${PROTOBUF_VERSION}.tar.gz && \
     echo "${PROTOBUF_CHECKSUM} protobuf-cpp-${PROTOBUF_VERSION}.tar.gz" | sha256sum -c - && \
     tar xzvf protobuf-cpp-${PROTOBUF_VERSION}.tar.gz && \
     cd protobuf-${PROTOBUF_VERSION} && \
@@ -43,30 +59,13 @@ curl -fsSLO https://github.com/protocolbuffers/protobuf/releases/download/v${PRO
 pip3 install google
 pip3 install protobuf
 
-rm /usr/local/include/boost/ -rf
-rm /usr/local/lib/libboost_* -f
-
-export BOOST_VERSION=1_78_0
-export BOOST_VERSION_DOT=1.78.0
-export BOOST_CHECKSUM=8681f175d4bdb26c52222665793eef08490d7758529330f98d3b29dd0735bccc
-rm -rf "boost_${BOOST_VERSION}"
-curl -fsSLO "https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION_DOT}/source/boost_${BOOST_VERSION}.tar.bz2" && \
-    echo "${BOOST_CHECKSUM} boost_${BOOST_VERSION}.tar.bz2" | sha256sum -c - && \
-    tar -xjf "boost_${BOOST_VERSION}.tar.bz2" && \
-    cd "boost_${BOOST_VERSION}" && \
-    ./bootstrap.sh --prefix=/usr/local && \
-    ./b2 --with-iostreams --with-date_time --with-filesystem --with-system --with-program_options --with-chrono --with-test -j$(nproc) install && \
-    cd .. && \
-    rm -rf "boost_${BOOST_VERSION}.tar.bz2" "boost_${BOOST_VERSION}"
-
 echo ""
 echo "Configuration done."
 echo ""
 echo "Please add to your environment:"
 echo ""
-echo "  export PATH=/usr/lib/llvm-11/bin:\$PATH"
+echo "  export PATH=/usr/lib/llvm-13/bin/:/usr/lib/llvm-11/bin:\$PATH"
 echo "  export CC=/usr/lib/llvm-13/bin/clang"
-echo "  export CXX=/usr/lib/llvm-13/bin/clang++"
 echo ""
 echo "Please build and install the following packages"
 echo ""
